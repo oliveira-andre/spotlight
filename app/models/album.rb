d59b9f38c0cd654349ee4564944c3e5c73eca941
oliveira-andre <desmonplays@gmail.com>
2019-12-19 19:17:10 -0400

    (new feature) active storage and validations

---
+++ app/models/album.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Album < ApplicationRecord
+  belongs_to :category
+  belongs_to :artist
+  has_many :songs
+
+  validates :title, presence: true
+  validates :date, presence: true
+
+  has_one_attached :cover
+end
