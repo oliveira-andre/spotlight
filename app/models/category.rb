9fde59a9f44213467d35e6f6e380cbb99ac78443
oliveira-andre <desmonplays@gmail.com>
2019-12-19 22:21:11 -0400

    (new feature) adding methods artists and songs

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
+  has_many :albums
+
   validates :name, presence: true
   has_one_attached :image
+
+  def artists
+    Artist.joins(:albums).where(albums: { id: albums.ids }).distinct
+  end
+
+  def songs
+    Song.joins(:album).where(albums: { id: albums.ids }).distinct
+  end
 end
