0fbbe313ceb7cf84d870fb74639b9a7ba65cba23
oliveira-andre <desmonplays@gmail.com>
2019-12-19 21:53:56 -0400

    (new feature) devise, has many favorites and recentely headers

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :favorites
+  has_many :recently_heards
+end
