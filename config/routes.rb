f801fcd615ce273795fda3bb2336a3123d7746c8
oliveira-andre <desmonplays@gmail.com>
2019-12-19 22:22:01 -0400

    (new feature) resourcing categories albums artists and categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,16 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#index'
+
+  authenticated :user do
+    root to: 'dashboard#index', as: :authenticated_root
+    resources :search, only: %i[index new], as: :searches
+    resources :categories, only: :show
+    resources :artists, only: :show
+    resources :albums, only: :show
+  end
+
+  unauthenticated :user do
+    root to: 'home#index'
+  end
 end
