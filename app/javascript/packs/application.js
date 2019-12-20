487f44b60703b5125a9fd15e6188aa151873812a
oliveira-andre <desmonplays@gmail.com>
2019-12-19 22:20:56 -0400

    (new feature) removing and adding the class is active to tab items

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,15 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+$ = require('jquery')
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+$(document).on("turbolinks:load", function () {
+  $("div.tabs.shared li").click(function () {
+    let target = $(this).children("a").prop("target");
+    $("div.tabs.shared li").removeClass("is-active");
+    $(this).addClass("is-active");
+    $("div.tabs-content div.tab-item").removeClass("active");
+    $(`div.tabs-content div${target}`).addClass("active");
+  });
+});
