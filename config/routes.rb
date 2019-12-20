a1ac44afe3f91645cb85ae66905b2a1a6c8d3efe
oliveira-andre <desmonplays@gmail.com>
2019-12-19 22:41:44 -0400

    (new feature) resourcing favorites

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,20 @@ Rails.application.routes.draw do
   devise_for :users
 
   authenticated :user do
+    resources :favorites, only: :index
     root to: 'dashboard#index', as: :authenticated_root
     resources :search, only: %i[index new], as: :searches
     resources :categories, only: :show
     resources :artists, only: :show
-    resources :albums, only: :show
+    resources :albums, only: :show do
+      resources :recently_heards, only: :create
+    end
+    resources :songs, only: [] do
+      post '/favorite', to: 'favorites#create', on: :member,
+                        defaults: { format: :js, favoritable_type: 'Song' }
+      delete '/favorite', to: 'favorites#destroy', on: :member,
+                          defaults: { format: :js, favoritable_type: 'Song' }
+    end
   end
 
   unauthenticated :user do
