c292f67b28764d6501b6557e84700b1c23dbfbbf
oliveira-andre <desmonplays@gmail.com>
2019-12-19 22:43:11 -0400

    (new feature) finalizing the project

---
+++ app/controllers/recently_heards_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class RecentlyHeardsController < ApplicationController
+  def create
+    @recently_heard = current_user.recently_heards.new(album_id: params[:album_id])
+    @recently_heard.save
+    head :ok
+  end
+end
