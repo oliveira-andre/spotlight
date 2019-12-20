9878672280b8b29a7244f9122dafe981ef39a508
oliveira-andre <desmonplays@gmail.com>
2019-12-19 22:26:42 -0400

    (new feature) where on songs, artists and albums

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class SearchController < ApplicationController
+  def index
+    @songs = Song.where('title LIKE ?', "%#{params[:search]}%")
+    @albums = Album.where('title LIKE ?', "%#{params[:search]}%")
+    @artists = Artist.where('name LIKE ?', "%#{params[:search]}%")
+  end
+
+  def new; end
+end
