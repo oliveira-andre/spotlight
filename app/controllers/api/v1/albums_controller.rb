58c6105f8974b6556d7bb71a91e0c1de423f4a12
Andre Oliveira <desmonplays@gmail.com>
2020-04-14 01:20:40 -0400

    (new feature) loading album

---
+++ app/controllers/api/v1/albums_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class AlbumsController < ApplicationController
+      before_action :load_album
+
+      def show; end
+
+      private
+
+      def load_album
+        @album = Album.find(params[:id])
+      end
+    end
+  end
+end
