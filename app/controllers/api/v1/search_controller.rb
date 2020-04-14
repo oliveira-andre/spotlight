a8326532d0938e296feb8e6b57f5840adb1220e4
Andre Oliveira <desmonplays@gmail.com>
2020-04-14 01:15:23 -0400

    (new feature) querying album song and artist

---
+++ app/controllers/api/v1/search_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    class SearchController < ApplicationController
+      def index
+        @songs = Song.where("title LIKE ?", "%#{params[:query]}%")
+        @albums = Album.where("title LIKE ?", "%#{params[:query]}%")
+        @artists = Artist.where("name LIKE ?", "%#{params[:query]}%")
+      end
+    end
+  end
+end
