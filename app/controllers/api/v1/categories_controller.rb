2b8d370315939121dcfc1b189a46009a165aa6e9
Andre Oliveira <desmonplays@gmail.com>
2020-04-14 01:14:32 -0400

    (new feature) loading category on show action

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,14 +1,21 @@
 module Api
   module V1
     class CategoriesController < ApplicationController
-      before_action :load_categories
+      before_action :load_categories, only: :index
+      before_action :load_category, only: :show
 
       def index; end
+
+      def show; end
 
       private
 
       def load_categories 
         @categories = Category.all
+      end
+
+      def load_category
+        @category = Category.find(params[:id])
       end
     end
   end
