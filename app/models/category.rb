badb2388c2f663b7a6cb8a30841e9f8eaafc1b31
Andre Oliveira <desmonplays@gmail.com>
2020-04-14 01:14:46 -0400

    (new feature) catching artists and songs

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,13 @@ class Category < ApplicationRecord
   validates :name, presence: true
 
   has_one_attached :image
+
+  def artists
+    Artist.joins(:albums).where(albums: { id: self.albums.ids }).distinct
+  end
+
+
+  def songs
+    Song.joins(:album).where(songs: { id: self.albums.ids }).distinct
+  end
 end
