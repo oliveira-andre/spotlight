72a57b53a866e8face1cbb0c62c855d2db925a24
Andre Oliveira <desmonplays@gmail.com>
2020-04-15 19:47:33 -0400

    (new feature) initializing favorites screen

---
+++ app/javascript/screens/favorites/index.js
@@ -0,0 +1,9 @@
+import React, { Fragment, useState } from 'react';
+
+export default FavoritesScreen = () => {
+  return(
+    <Fragment>
+      <div>Favorites Screen</div>
+    </Fragment>
+  );
+}
