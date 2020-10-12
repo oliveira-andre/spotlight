89b937aab60e5114be8e9e9f922c741767dda63c
Andre Oliveira <desmonplays@gmail.com>
2020-10-12 20:35:40 -0300

    feat: applying frozen string literal to true

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   protect_from_forgery unless: -> { request.format.json? }
+
+  before_action :authenticate_user!, unless: -> { request.env['PATH_INFO'] == '/' }
 end
