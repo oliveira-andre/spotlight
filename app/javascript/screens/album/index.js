55293f86097de205d30a85bfcf4a96d20679317b
Andre Oliveira <desmonplays@gmail.com>
2020-10-12 22:42:38 -0300

    feat: rendering albums and serction wrapper

--- app/javascript/screens/album/index.js
+++ app/javascript/screens/album/index.js
@@ -1,11 +1,16 @@
 import React, { Fragment } from 'react';
 import NavbarFooter from '../../components/common/navbar_footer';
+import Albums from '../../components/albums';
+import SectionWrapper from '../../components/common/section_wrapper';
 
 const AlbumScreen = () => {
-  return(
-    <Fragment>
-      <NavbarFooter/>
-    </Fragment>
-  );
+ return(
+   <Fragment>
+     <SectionWrapper>
+       <Albums/>
+       <NavbarFooter/>
+     </SectionWrapper>
+   </Fragment>
+);
 }
 export default AlbumScreen;
