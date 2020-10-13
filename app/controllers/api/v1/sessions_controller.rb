ea9c79199e898cebad027a9ddbcb6baee252b16d
Andre Oliveira <desmonplays@gmail.com>
2020-10-13 17:09:22 -0300

    feat: skiping the auth before action

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class SessionsController < ApplicationController
+      skip_before_action :authenticate_user!
+      before_action :load_user
+
+      def create
+        raise ServiceException unless @user.valid_password?(
+          session_params[:password]
+        )
+
+        head :created
+      end
+
+      private
+
+      def session_params
+        params.require(:session).permit(:email, :password)
+      end
+
+      def load_user
+        @user = User.find_by(email: session_params[:email])
+      end
+    end
+  end
+end
