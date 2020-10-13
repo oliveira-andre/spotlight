8762f50443b75939daa3c6e5e069443603b7aa21
Andre Oliveira <desmonplays@gmail.com>
2020-10-13 17:06:11 -0300

    fix: removing devise, initializing on hand password encryptation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,39 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  include BCrypt
+
+  has_secure_password
 
   has_many :favorites
   has_many :recently_heards
 
   validates :email, :password, :name, presence: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(value)
+    @password = Password.create(value)
+    self.password_digest = @password
+  end
+
+  def valid_password?(password)
+    Password.new(password_digest) == password
+  end
+
+  def token
+    JWT.encode(
+      {
+        data: {
+          id: id,
+          name: name,
+          email: email
+        }
+      }, JWT_SECRET, 'HS256'
+    )
+  end
 
   def favorite?(kind, id)
     favorites.where(favoritable_type: kind, favoritable_id: id).present?
---
+++ config/initializers/constants.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+JWT_SECRET = ENV['jwt_secret'] || 'secret123'
