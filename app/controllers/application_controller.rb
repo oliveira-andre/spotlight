9878c44d3a2f60ba7fc115fe3bbfaaa670a2fa5c
Andre Oliveira <desmonplays@gmail.com>
2020-10-12 21:23:07 -0300

    feat: after login redirect to discovery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
 
   before_action :authenticate_user!, unless: -> { request.env['PATH_INFO'] == '/' }
+
+  def after_sign_in_path_for(_resource)
+    '/discovery'
+  end
 end
