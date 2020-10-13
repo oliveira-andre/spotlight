226b331a8008c0bbed9ea364a8a706c67e7bb51a
Andre Oliveira <desmonplays@gmail.com>
2020-10-12 22:41:58 -0300

    feat: removing mock and adding fetch hooks of albums

--- app/javascript/components/discovery/index.js
+++ app/javascript/components/discovery/index.js
@@ -1,26 +1,38 @@
-import React, { Fragment } from 'react';
-import Album from '../album';
+import React, { Fragment, useEffect, useState } from 'react';
+import Album from '../common/album';
 import styled from 'styled-components';
 import { Columns, Heading } from 'react-bulma-components';
+import AlbumsService from '../../services/albums';
 
 const DivVSpaced = styled.div`
   margin-top: 50px;
 `;
 
-const albums_mock = [
-  { id: 1, artist_name: 'Andrew Howes', title: 'Gubernator', cover_url: '/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--799864becde6954163165f4d5fda4c6f733ad6d0/gubernator.jpg' },
-  { id: 2, artist_name: 'Andrew Howes', title: 'The Great Bear', cover_url: '/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBFQT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--bfd67a7563fd2a34c5f1deafd4e94a8ba63b541c/the_great_bear.jpg' },
-  { id: 3, artist_name: 'Yellow Chair', title: 'Barcelona', cover_url: '/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBGUT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--a49bdb542106a0dc6a5cb71850920e71df8d4672/barcelona.jpg' },
-  { id: 4, artist_name: 'Andrew Howes', title: 'Gubernator', cover_url: '/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--799864becde6954163165f4d5fda4c6f733ad6d0/gubernator.jpg' },
-];
-
-const albums = albums_mock.map((album, key) =>
-  <Columns.Column desktop={{ size: 3 }} mobile={{ size: 6 }} key={key}>
-    <Album artist_name={album.artist_name} title={album.title} cover_url={album.cover_url} id={key}/>
-  </Columns.Column>
-);
-
 const Discovery = () => {
+  const [recent_albums, setRecentAlbums] = useState([]);
+  const [recommended_albums, setRecommendedAlbums] = useState([]);
+
+  async function fetchAlbums() {
+    const response = await AlbumsService.index();
+    setRecentAlbums(response.data['recent_albums'])
+    setRecommendedAlbums(response.data['recommended_albums'])
+  }
+  useEffect(() => {
+     fetchAlbums();
+  }, []);
+
+  const recent_albums_components = recent_albums.map((album, key) =>
+    <Columns.Column desktop={{ size: 3 }} mobile={{ size: 6 }} key={album}>
+      <Album artist_name={album.artist_name} title={album.title} cover_url={album.cover_url} key={key} id={album.id}/>
+    </Columns.Column>
+  );
+
+  const recommended_albums_components = recommended_albums.map((album, key) =>
+    <Columns.Column desktop={{ size: 3 }} mobile={{ size: 6 }} key={key}>
+      <Album artist_name={album.artist_name} title={album.title} cover_url={album.cover_url} key={key} id={album.id}/>
+    </Columns.Column>
+  );
+
   return (
     <Fragment>
       <div>
@@ -28,7 +40,7 @@ const Discovery = () => {
           Tocadas recentemente
         </Heading>
         <Columns className='is-mobile'>
-          {albums}
+          {recent_albums_components}
         </Columns>
       </div>
 
@@ -37,7 +49,7 @@ const Discovery = () => {
           Recomendadas
         </Heading>
         <Columns className='is-mobile'>
-          {albums}
+          {recommended_albums_components}
         </Columns>
       </DivVSpaced>
     </Fragment>
