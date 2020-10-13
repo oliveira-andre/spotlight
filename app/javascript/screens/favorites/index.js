f61abbb69d02afb31d436bd52245354222ec023e
Andre Oliveira <desmonplays@gmail.com>
2020-10-13 01:05:57 -0300

    feat: adding favorite common

--- app/javascript/screens/favorites/index.js
+++ app/javascript/screens/favorites/index.js
@@ -1,9 +1,17 @@
 import React, { Fragment, useState } from 'react';
+import { Heading } from 'react-bulma-components';
+
 import NavbarFooter from '../../components/common/navbar_footer';
+import SectionWrapper from '../../components/common/section_wrapper';
+import Favorites from '../../components/favorites';
 
 const FavoritesScreen = () => {
   return(
     <Fragment>
+      <SectionWrapper>
+        <Heading className='has-text-centered has-text-white'>Favoritos</Heading>
+        <Favorites />
+      </SectionWrapper>
       <NavbarFooter/>
     </Fragment>
   );
