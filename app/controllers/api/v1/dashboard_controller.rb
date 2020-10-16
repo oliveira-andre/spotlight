ce8bfaa96737a8336198caad96342ba01791e75b
Andre Oliveira <desmonplays@gmail.com>
2020-10-15 22:42:18 -0300

    feat: load recent without distinct

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -3,27 +3,22 @@
 module Api
   module V1
     class DashboardController < ApplicationController
-      before_action :load_recent_heard, only: :index
+      before_action :load_recent_albums, only: :index
       before_action :load_recommendations, only: :index
 
       def index; end
 
       private
 
-      def load_recent_heard
-        @recent_albums = @current_user.recently_heards
-                                     .order('created_at DESC')
-                                     .limit(8).map(&:album).uniq
+      def load_recent_albums
+        @recent_albums = @current_user.recently_heards.albums
       end
 
       def load_recommendations
         heard_categories = @recent_albums.map(&:category)
         @recommended_albums = if heard_categories.present?
-                                Album.joins(:category, :songs)
-                                     .where(category: heard_categories)
-                                     .order('songs.played_count')
-                                     .select('distinct albums.*')
-                                     .limit(12)
+                                Album.joins(:category, :songs).where(category: heard_categories)
+                                     .order('songs.played_count').select('albums.*').limit(12)
                               else
                                 @recommended_albums = Album.all.limit(12)
                               end
