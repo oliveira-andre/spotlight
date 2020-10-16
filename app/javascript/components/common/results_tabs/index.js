a81d192a483dceb31cd2893ee2ef8408722bb244
Andre Oliveira <desmonplays@gmail.com>
2020-10-15 22:42:51 -0300

    fix: when hover tabs border and color is white

--- app/javascript/components/common/results_tabs/index.js
+++ app/javascript/components/common/results_tabs/index.js
@@ -13,6 +13,11 @@ const CustomTab = styled(Tabs.Tab)`
       border-color: hsl(171, 100%, 41%) !important;
     `}
   }
+
+  a:hover {
+    color: white;
+    border-color: white;
+  }
 `
 
 const ResultsTabs = (props) => {
